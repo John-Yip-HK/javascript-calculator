8dc668597d5f3764fd52893caeaf8457be21bb06
John-Yip-HK <yip4work@gmail.com>
2021-11-27 14:59:14 +0800

    Touched up keypad interface.

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -3,7 +3,7 @@ import Col from "react-bootstrap/Col";
 
 export default function Display() {
   return (
-    <Row id="display-container">
+    <Row className="row-container" id="display-container">
       <Col id="display">012345678</Col>
       <hr />
       <Col id="operand-and-operator-display"></Col>
--- src/components/KeypadComponent.js
+++ src/components/KeypadComponent.js
@@ -4,52 +4,70 @@ import Col from "react-bootstrap/Col";
 
 export default function Keypad() {
   return (
-    <Row>
+    <Row className="row-container" id="keypad-container">
       <Container id="keypad">
         <Row>
-          <Col xs={6} id="clear">
+          <Col xs={6} id="clear" className="clickable">
             AC
           </Col>
-          <Col id="divide" className="operand">
+          <Col id="divide" className="operand clickable">
             /
           </Col>
-          <Col id="multiply" className="operand">
+          <Col id="multiply" className="operand clickable">
             *
           </Col>
         </Row>
         <Row>
-          <Col id="seven">7</Col>
-          <Col id="eight">8</Col>
-          <Col id="nine">9</Col>
-          <Col id="subtract" className="operand">
+          <Col id="seven" className="clickable">
+            7
+          </Col>
+          <Col id="eight" className="clickable">
+            8
+          </Col>
+          <Col id="nine" className="clickable">
+            9
+          </Col>
+          <Col id="subtract" className="operand clickable">
             -
           </Col>
         </Row>
         <Row>
-          <Col id="four">4</Col>
-          <Col id="five">5</Col>
-          <Col id="six">6</Col>
-          <Col id="add" className="operand">
+          <Col id="four" className="clickable">
+            4
+          </Col>
+          <Col id="five" className="clickable">
+            5
+          </Col>
+          <Col id="six" className="clickable">
+            6
+          </Col>
+          <Col id="add" className="operand clickable">
             +
           </Col>
         </Row>
         <Row>
-          <Col xs={9} className="no-border">
+          <Col xs={9} className="no-border nested-row">
             <Row>
-              <Col id="one">1</Col>
-              <Col id="two">2</Col>
-              <Col id="three">3</Col>
+              <Col id="one" className="clickable">
+                1
+              </Col>
+              <Col id="two" className="clickable">
+                2
+              </Col>
+              <Col id="three" className="clickable">
+                3
+              </Col>
             </Row>
             <Row>
-              <Col xs={8} id="zero">
+              <Col xs={8} id="zero" className="clickable">
                 0
               </Col>
-              <Col xs={4} id="decimal">
+              <Col id="decimal" className="clickable">
                 .
               </Col>
             </Row>
           </Col>
-          <Col xs={3} id="equals">
+          <Col id="equals" className="clickable">
             =
           </Col>
         </Row>
